3417820b07792fb488b4c7e2c3552b55608e324e
dallel-saied <dallel11@gmail.com>
2021-12-11 11:31:16 +0100

    update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 import "dotenv/config"
 
 //const MONGODB_URL = "mongodb://localhost:27017/task-db"
+
 const MONGODB_URL = `mongodb://${process.env.DATABASE_HOST}:
 ${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`
 
